6ddc8eed95bfb02edb52946db897dd112dc9f70f
bikram Pal <palbikram557@gmail.com>
2025-12-28 02:35:56 +0530

    after add the gsap

--- script.js
+++ script.js
@@ -20,6 +20,19 @@ const sections = document.querySelectorAll("div[id]");
 // toggle menu (mobile)
 menuBtn.addEventListener("click", () => {
   navList.classList.toggle("open");
+  gsap.from(navList,{
+    opacity:0,
+    y:20,
+    duration:1,
+  })
+  gsap.from(menuBtn,{
+      scale:0,
+      duration:1,
+      y:0,
+      // yoyo:true,
+    })
+    
+
   menuBtn.classList.toggle("open");
 });
 
@@ -28,8 +41,8 @@ navLinks.forEach(link => {
   link.addEventListener("click", () => {
     navLinks.forEach(l => l.classList.remove("active"));
     link.classList.add("active");
-
     navList.classList.remove("open");
+
     menuBtn.classList.remove("open");
   });
 });
@@ -57,3 +70,191 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+tl=gsap.timeline()
+tl.from(".logo",{
+  opacity:0,
+  x:"-20px",
+  duration:0.4,
+  delay:0.5,
+})
+tl.from(".nav-element",{
+  opacity:0,
+  y:"20px",
+  stagger:0.2,
+  duration:1,
+  // delay:1,
+})
+tl.from(".home-about div h1",{
+  opacity:0,
+  y:"-20px",
+  stagger:0.2,
+  duration:0.4,
+  delay:-0.5,
+})
+ 
+const textEl = document.querySelector(".home-about>h2")
+const text = textEl.textContent;
+
+textEl.textContent = ""; // clear text first
+
+tl.to({}, {
+  duration: text.length * 0.08, // typing speed
+  ease: "none",
+  onUpdate: function () {
+    const progress = Math.floor(this.progress() * text.length);
+    textEl.textContent = text.substring(0, progress);
+  }
+});
+tl.from(".home-about>h4",{
+  duration:1,
+  opacity:0,
+  x:0,
+  delay:0.2,
+  scaleX:0.9,
+})
+gsap.from(".home-img",{
+  opacity:0,
+  
+  x:"80px",
+  scale:1.2,
+  duration:0.5,
+  delay:1,
+})
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.create({
+  trigger: ".about",
+  start: "top 70%",
+  // markers: true,
+
+  onEnter: () => {
+    gsap.to(".home-about", {
+      opacity: 0,
+      x: -40,
+      duration: 0.4,
+      ease: "power2.out"
+    });
+    gsap.to(".home-img", {
+      opacity: 0,
+      x: 40,
+      duration: 0.5,
+      ease: "power2.out"
+    });
+    
+  },
+
+  
+
+  onLeaveBack: () => {
+    gsap.to(".home-about", {
+      opacity: 1,
+      x: 0,
+      duration: 0.4,
+      ease: "power2.out"
+    });
+     gsap.to(".home-img", {
+      opacity: 1,
+      x: 0,
+      duration: 0.4,
+      ease: "power2.out"
+    });
+  }
+});
+
+const aboutTl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".about",
+    start: "top 50%",
+    // once: true,
+    // markers:true,
+  }
+});
+
+aboutTl
+  .from(".about h2", {
+    opacity: 0,
+    y: 50,
+    duration: 0.6
+  })
+  .from(".my-img", {
+    opacity: 0,
+    scale: 0.8,
+    duration: 0.6
+  }, "-=0.3")
+  .from(".content-about .content", {
+    opacity: 0,
+    y: 30,
+    stagger: 0.2,
+    duration: 0.6
+  }, "-=0.3");
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+ScrollTrigger.create({
+  trigger: ".contact",
+  start: "top 70%",
+  // markers: true,
+
+  onEnter: () => {
+    gsap.to(".content-about", {
+      opacity: 0,
+      x: -40,
+      duration: 0.4,
+      ease: "power2.out"
+    });
+    gsap.to(".my-img", {
+      opacity: 0,
+      x: 40,
+      duration: 0.5,
+      ease: "power2.out"
+    });
+    
+  },
+
+  
+
+  onLeaveBack: () => {
+    gsap.to(".content-about", {
+      opacity: 1,
+      x: 0,
+      duration: 0.4,
+      ease: "power2.out"
+    });
+     gsap.to(".my-img", {
+      opacity: 1,
+      x: 0,
+      duration: 0.4,
+      ease: "power2.out"
+    });
+  }
+});
+
+const contactt1 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".contact",
+    start: "top 30%",
+    // once: true,
+    // markers:true,
+  }
+});
+
+aboutTl
+  .from(".contact h2", {
+    opacity: 0,
+    y: 50,
+    duration: 0.6
+  })
+  .from(".contect-img", {
+    opacity: 0,
+    x:50,
+    duration: 1
+  })
+  .from(".contact-content", {
+    opacity: 0,
+    y: 30,
+    stagger: 0.2,
+    duration: 0.6
+  }, "-=0.3");
+
+
